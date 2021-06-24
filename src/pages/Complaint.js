8b4eb178b79dac221d68458150c168bc9fdab367
Ayboss <bamiayo90@gmail.com>
2021-06-24 19:47:11 +0100

    continous

--- src/pages/Complaint.js
+++ src/pages/Complaint.js
@@ -1,56 +1,75 @@
-import React from 'react';
+import React, {useState} from 'react';
 import user from '../Assets/users.jpg'
 import admin from '../Assets/admin.jpg';
 import send from '../Assets/send.svg';
 import '../css/complaint.css'
 
 function Complaint() {
+    const [open, setOpen]= useState(false)
     return (
-        <div className="complaint">
-            <div className="complaint__open">
-                <h3>Open Tickets</h3>
+        <div className="complaint__body">
+            <div className="complaint__stats">
+                <div className="complaint__tab">
+                    <a onClick={()=>setOpen(true)} className={`complaint__tab--btn ${open?'complaint__tab--btnActive':''}`}>Open</a>
+                    <a onClick={()=>setOpen(false)} className={`complaint__tab--btn ${!open?'complaint__tab--btnActive':''}`}>Close</a>
+                </div>
                 <ul className="complaint__list">
-                    <li className="complaint__chat">
-                        <div className="complaint__user">
-                            <div className="complaint__img"><img src={user} alt="user"/></div>
-                            <p>Lorem ipsum dolor sit amet consectetur adi</p>
+                    <li className="complaint__item">
+                        <img src={user} alt="user" className="complaint__userImg"/>
+                        <div className="complaint__itemDetails">
+                            <h3>John Doe</h3>
+                            <p>lorem ipsum and stuffs ...</p>
                         </div>
-                        <div className="complaint__admin">
-                            <p>Lorem ipsum dolor sit amet consectetur adi</p>
-                            <div className="complaint__img"><img src={admin} alt="admin"/></div>
-                        </div>
-                        <div className="complaint__reply">
-                            <input placeholder="type something..." className="complaint__input"/><button className="complaint__send"><img src={send} alt="send"/></button>
-                        </div>
-                        <div className="compliant__"><button>Satisfied and Close</button></div>
                     </li>
-                    <li className="complaint__chat">
-                        <div className="complaint__user">
-                            <div className="complaint__img"><img src={user} alt="user"/></div>
-                            <p>Lorem ipsum dolor sit amet consectetur adi</p>
+                    <li className="complaint__item">
+                        <img src={user} alt="user" className="complaint__userImg"/>
+                        <div className="complaint__itemDetails">
+                            <h3>John Doe</h3>
+                            <p>lorem ipsum and stuffs ...</p>
                         </div>
-                        <div className="complaint__admin">
-                            <p>Lorem ipsum dolor sit amet consectetur adi</p>
-                            <div className="complaint__img"><img src={admin} alt="admin"/></div>
-                        </div>
-                        <div className="complaint__reply">
-                            <input placeholder="type something..." className="complaint__input"/><button className="complaint__send"><img src={send} alt="send"/></button>
-                        </div>
-                        <div className="compliant__"><button>Satisfied and Close</button></div>
                     </li>
-                
-                </ul>
-            </div> 
-            <div className="complaint__close">
-                <h3>Closed Tickets</h3>
-                <ul>
-                    <li>
-
+                    <li className="complaint__item">
+                        <img src={user} alt="user" className="complaint__userImg"/>
+                        <div className="complaint__itemDetails">
+                            <h3>John Doe</h3>
+                            <p>lorem ipsum and stuffs ...</p>
+                        </div>
                     </li>
                 </ul>
             </div>
-        </div>
+            <div className="complaint__graphs">
+                <div className="complaint">
+                    <div className="complaint__header">
+                        <img className="complaint__img" src={user} alt="user"/>
+                        <h3>John Doe</h3>
+                        <button>Mark As Closed</button>
+                    </div>
+                    <ul className="complaint__chat">
+                        <li className="complaint__user">
+                            <p>Lorem ipsum dolor sit amet consectetur adi</p>
+                        </li>
+                        <li className="complaint__admin">
+                            <p>Lorem ipsum dolor sit amet consectetur adi</p>
+                        </li>
+                        <li className="complaint__admin">
+                            <p>Lorem ipsum dolor sit amet consectetur adi</p>
+                        </li>
+                        <li className="complaint__admin">
+                            <p>Lorem ipsum dolor sit amet consectetur adi</p>
+                        </li>
+                        <li className="complaint__admin">
+                            <p>Lorem ipsum dolor sit amet consectetur adi</p>
+                        </li>
+                        <li className="complaint__admin">
+                            <p>Lorem ipsum dolor sit amet consectetur adi</p>
+                        </li>
+                    </ul>
+                    <div className="complaint__reply">
+                        <input placeholder="type something..." className="complaint__input"/><button className="complaint__send"><img src={send} alt="send"/></button>
+                    </div>
+                </div>
+            </div>
+        </div>       
     )
 }
-
 export default Complaint
