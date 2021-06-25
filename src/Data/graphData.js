1e3df3701b6a526043c1d2c962ce9624c1cbe7cd
Ayboss <bamiayo90@gmail.com>
2021-06-25 06:22:03 +0100

    netlify

---
+++ src/Data/graphData.js
@@ -0,0 +1,60 @@
+export const accountYear = {
+    accountLimit: {
+        color: 'red',
+        data:[500,520,600,680,700,750,800,970,1002,1240,1700,2000]
+    },
+    creditLimit: {
+        color: 'blue',
+        data:[500,540,620,680,700,850,900,940,1302,1640,2300,2500]
+    },
+    creditBalance: {
+        color: 'green',
+        data:[700,820,900,980,1300,1750,1800,1970,2002,2240,2700,3200]
+    },
+    statementCredit: {
+        color: 'gold',
+        data:[70,120,200,210,230,250,270,302,322,400,430,500]
+    },
+    defaultAmount: {
+        color: 'pink',
+        data:[1500,1620,1900,1980,2300,2750,2800,2970,3102,3240,3700,4300]
+    }
+}
+export const accountMonth ={
+    accountLimit: {
+        color: 'red',
+        data:[700,701,701,702,703,705,706,710,711,712,
+            713,715,715,716,716,716,718,719,722,729,729,
+            730,730,731,732,734,736,737,737,740,750]
+    },
+    creditLimit: {
+        color: 'blue',
+        data:[4,3,10,23,25,30,30,45,52,41,55,60]
+    },
+    creditBalance: {
+        color: 'green',
+        data:[2,6,7,21,22,33,45,45,51,56,58,66]
+    },
+    statementCredit: {
+        color: 'gold',
+        data:[2,6,7,21,22,33,45,45,51,56,58,66]
+    },
+    defaultAmount: {
+        color: 'pink',
+        data:[2,6,7,21,22,33,45,45,51,56,58,66]
+    }
+}
+export const  usersYear = {
+    accountHolder: {
+        color: '#00ff26',
+        data:[2,6,7,21,22,33,45,45,51,56,58,66]
+    },
+    newsletter: {
+        color: '#9d7219',
+        data:[2,6,7,21,22,33,45,45,51,56,58,66]
+    },
+    openComplaints: {
+        color: '#c700d9',
+        data:[2,6,7,21,22,33,45,45,51,56,58,66]
+    }
+}
