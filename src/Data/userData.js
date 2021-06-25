c4af87d01e7912aaf40fd34e43db72ec96181750
Ayboss <bamiayo90@gmail.com>
2021-06-25 14:05:53 +0100

    dropdown

--- src/Data/userData.js
+++ src/Data/userData.js
@@ -1,6 +1,17 @@
 
 const data = [
-    {name:"Bisola Doe"},
-    {phone:23489257929},
-    {email: 'delinquencent'}
-]
+    {name:"Bisola Doe",
+    phone:23489257929,
+    email: 'delinquencent',
+    address:'2, bintu alli street',
+    gender:'male',
+    employmentType: "employee",
+    _id:"24523452jk2535623",
+    trustScore: 30,
+    accountStatus: "pending",
+    accountLimit: 20000,
+    creditLimit: 3000,
+    creditBalance: 200,
+    statementCredit: 5000,
+    referralID: "34j32wu93403424"}
+    ]
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavLink} from 'react-router-dom';
 import '../css/header.css';
 
 function Header() {
+    const [toggle, setToggle] = useState(false);
+    const handleLabel = ()=>{
+        setToggle(!toggle)
+    }
     return (
         <header className="header">
             <span className="header__logo">
-                <div class="logo"></div>Loanith</span>
-            <ul className="header__navList">
+                <div class="logo"></div>Loanith
+            </span>
+            <label onClick={handleLabel} className="togglebtn"><div></div></label>
+            <ul className={`header__navList ${!toggle?'header__logo--show':''}`}>
                 <li className="header__navItem"><NavLink activeClassName="header__link--active" className="header__link" exact to="/">Home</NavLink></li>
                 <li className="header__navItem"><NavLink activeClassName="header__link--active" className="header__link" to="/transactions">Transactions</NavLink></li>
                 <li className="header__navItem"><NavLink activeClassName="header__link--active" className="header__link" to="/users">Users</NavLink></li>
