00a8f31d712c5fb9588ce67a514efe14af9c799f
Ayboss <bamiayo90@gmail.com>
2021-06-25 10:38:07 +0100

    logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,8 @@ import '../css/header.css';
 function Header() {
     return (
         <header className="header">
-            <span className="header__logo">Loanith</span>
+            <span className="header__logo">
+                <div class="logo"></div>Loanith</span>
             <ul className="header__navList">
                 <li className="header__navItem"><NavLink activeClassName="header__link--active" className="header__link" exact to="/">Home</NavLink></li>
                 <li className="header__navItem"><NavLink activeClassName="header__link--active" className="header__link" to="/transactions">Transactions</NavLink></li>
